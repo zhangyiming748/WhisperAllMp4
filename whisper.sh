1fbf0c7e75628a53a03401ec668be95e556f0f21
zen <zhangyiming748@gmail.com>
2024-08-14 16:04:39 +0800

    再次尝试

--- whisper.sh
+++ whisper.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 # 用于查找指定路径下和子文件夹中所有扩展名为 MP4 的文件,并分离出当前视频的绝对路径(不包含文件名)如果当前路径下不存在扩展名为 srt 的文件,则执行whisper
 # 设置要搜索的根目录
 root_dir="/data"
-
+model_dir="/data"
+echo "root = ${root_dir}"
+echo "model_dir = ${model_dir}"
 # 遍历根目录及其子目录
 find "$root_dir" -type f -name "*.mp4" | while read file; do
     # 获取文件的绝对路径
